29196f5608c4e32e9006a965a6014753b20359f1
Laasli <homeref@gmail.com>
2015-04-13 19:08:45 +0200

    such version bump, so even, much odd, WOW very reverse DOGE

--- dogepy/dogepy/__init__.py
+++ dogepy/dogepy/__init__.py
@@ -26,3 +26,32 @@ _.._ :`4MM$!YYYYYYYYYii,.__.diii$$YYYYYYYYYYY
      :YYiii$$$$$$$YYYYYYY$$$$YY$$$$YYiiiiiYYYYYYi'
     \033[0m"""
     print(soString)
+
+def iicsAhcuMegoDhcus():
+    os.system('cls' if os.name == 'nt' else 'clear');
+    gnirtSos = """\033[93m
+          _                      .Y
+        .```.                   LiY
+       .`;;. ;.      WOW       ;iiY
+       : ;;;;.`;.           _.ii;YY
+       ;;;;::;; `i;;;;iiiiiYYYYYYii
+       ;;;   .;;.  iiYYYiiiiiiYYYYYY;._
+       ;;;;  `;iiYYYYYYYYYYYYiiYYYYYYYYYY.
+       ..`;ii.;YYYYYYiii$$$$YYiiYY$$YYYYYYY.
+      .iiYYiYYYYiiYYYYYYY$$$$$YYiiYT  .!$YYY:
+     .YYiiYYYYiiiYYYYY4"``"!$YYYYYY:   :$MM$Y
+      YYiYYYiiYllYYYY:   ': "iiY$$YYb,.d$$MM: .`
+      YYYYYYYYYYY$$iiib.__.,iiYYYYYYYYY!$MM4`: _.._
+     ;YiY$$$$$$YY$$$$iiiiiiii$$$$$4"     `P`d$ _.,.
+    LYYiiYY$$$$$$$$$YYYYY$$$$$$$$$:       :$`.,`
+   :UYYiiiYYYYYY$$$$YY$$T``.,.`$dPP;.    .$$;`"
+   YYiYYiiiYYY$$YY$Y$$$;d$$$Y!"$y$i;;;; :;;$P$;
+   YYYiYiiYYY$$$$$YY$$YYYYY:"-`.iii:`` .. $$iF$
+  :YYiYYYYYYY$$$$$$$YY$YYY;i;;.._`  ```` dr$$Y:
+ .YYYYiiYYYYYYY$$$$$$YYYYYYYYYYbiiiii;;i$$$$$:
+ YYYYYYiiiYYYYYYYY$$$$$$YYYYYYYYYYYYY$$$$$$$`
+ YYYYYYYiiiiiiYY$$YYY$$$$$$YYYYYYYYY$$$$$$!i.
+ 'iYYYYYYiiiiiYY$$$$YY$$$$YYYYYYY$$$$$$$iiiYY:
+    \033[0m"""
+    print(gnirtSos)
+
--- dogepy/setup.py
+++ dogepy/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='dogepy',
-      version='14.4',
+      version='46.3',
       description='So doge, much art, wow',
       url='https://github.com/laasli/doge',
       author='Laasli, Morz',
